2332fffa79806f0d54ae001af03df47bc9ad653b
Dylan Folscroft <dfolscroft805@gmail.com>
2025-08-08 08:57:53 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# gather input
+print('Complete the following prompts to fill out the madlib:')
+blank1 = input('Color:')
+blank2 = input('Verb-ed:')
+blank3 = input('Number:')
+blank4 = input('Animal:')
+blank5 = input('Adjective:')
+blank6 = input('Noun (Tool):')
+blank7 = input('Noun:')
+blank8 = input('Noun:')
+blank9 = input('Color:')
+blank10 = input('Noun:')
+blank11 = input('Noun:')
+blank12 = input('Noun:')
+blank13 = input('Noun:')
+blank14 = input('Verb-ed:')
+blank15 = input('Noun:')
+blank16 = input('Plural Noun:')
+blank17 = input('Color:')
+blank18 = input('Plural Noun:')
+blank19 = input('Plural Noun:')
+
+# display result
+print("\n")
+madlib = f"Today we had a substitute teacher for science class, with {blank1} hair that {blank2} straight up {blank3} \
+inches high.  His name was Mr.{blank4} and he said\nhe'd show us why science was the most {blank5} class.  First, he \
+used a {blank6} and a {blank7} to make a {blank8} of water turn {blank9}.\nThen he made a {blank10} of the solar system\
+using {blank11}, {blank12}, and {blank13}.  When the principal {blank14} by and saw the substitute teacher\nusing \
+{blank15} to turn {blank16} into {blank17} {blank18}, she asked him to show the class a movie about {blank19} instead. \
+The next day we had a different substitute teacher."
+
+print(madlib)
+#  completed script
